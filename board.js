00fab9156299d1dd090b660b342e2a9e6400f79f
Mariy <m.lebadina@gmail.com>
2022-06-01 19:48:33 +0300

    add 3d+collection

--- board.js
+++ board.js
@@ -1,81 +1,94 @@
-
 class Figure {
 
-    constructor (x, y, width, height) {
+    constructor (x, y, z, width, height, length) {
         this.width = width;
         this.height = height;
+        this.length = length;
         this.x = x;
         this.y = y;
+        this.z = z;
+        this.r = 0;//радиус поворота, deg
         this.currentSpeedRes = 0;
         this.currentSpeedX = 0;
         this.currentSpeedY = 0;
+        this.currentSpeedZ = 0;
         this.a = 0;
-        this.interval;
-
+        this.interval;//периодичность возвращения данных при движении
     }
     
     //Общая функция для движения
-    //from, to - принимает {x:'значение', y:'значение'}
-    //speedX, speedY - принимает начальные значения скорости по x и y
+    //from, to - принимает {x:'значение', y:'значение', z:'занчение'}
+    //speedX, speedY, speedZ - принимает начальные значения скорости по x и y
     //a - ускорение
-
-
-    move = (from, to, speedX, speedY, a = 0) => {
-        console.log('start');
+    move = (from, to, speedX, speedY, speedZ, a = 0) => {
         let t = 0;
         this.a = a;
-        console.log(speedX, speedY);
-        let speedRes = Math.pow((Math.pow(speedX, 2) + Math.pow(speedY, 2)), 0.5);
+        let speedRes = Math.pow((Math.pow(speedX, 2) + Math.pow(speedY, 2) + Math.pow(speedZ, 2)), 0.5);
         let aX = a*speedX/speedRes; //ускорение по х
         let aY = a*speedY/speedRes;//ускорение по у
+        let aZ = a*speedZ/speedRes;//ускорение по z
         this.interval = setInterval(() => {
             this.currentSpeedX = speedX + t*aX*0.001; //скорость по x в момент времени t
             this.currentSpeedY = speedY + t*aY*0.001; //скорость по y в момент времени t
-            this.currentSpeedRes = Math.pow((Math.pow(this.currentSpeedX, 2) + Math.pow(this.currentSpeedY, 2)), 0.5);
-            this.x = from.x + this.currentSpeedX*t; //координата по x в момент времени t
-            this.y = from.y + this.currentSpeedY*t; //координата по y в момент времени t
+            this.currentSpeedZ = speedZ + t*aZ*0.001; //скорость по z в момент времени t
+            this.currentSpeedRes = Math.pow((Math.pow(this.currentSpeedX, 2) +
+                                   Math.pow(this.currentSpeedY, 2) +
+                                   Math.pow(this.currentSpeedZ, 2)), 0.5);            
+            this.x = from.x + this.currentSpeedX*t*0.001; //координата по x в момент времени t
+            this.y = from.y + this.currentSpeedY*t*0.001; //координата по y в момент времени t
+            this.z = from.z + this.currentSpeedZ*t*0.001; //координата по z в момент времени t
             t += 1;
 
             if ((this.currentSpeedX > 0 && (this.x >= to.x) ||
                 (this.currentSpeedX < 0 && (to.x >= this.x)))){ //завершение функции если текущие координаты совпали с требуемыми
-               clearInterval(this.interval);
+               this.stopMovement();
                this.currentSpeedRes = 0;
                this.currentSpeedX = 0;
                this.currentSpeedY = 0;
+               this.currentSpeedZ = 0;
             }
-            return [this.x, this.y, this.currentSpeedRes]; //каждую милисекунду возвращает текущие координаты x, y и результирующую скорость
+            return [this.x, this.y, this.z, this.currentSpeedRes]; //возвращает текущие координаты x, y, z и результирующую скорость
         }, 1);
     }
 
     //Функция для равноускоренного движения
-    //from, to - принимает {x:'значение', y:'значение'}
+    //from, to - принимает {x:'значение', y:'значение', z: 'значение'}
     //duration - принимает время движения в секундах
 
     moveToEvenly = (from, to, duration) => {
         let speedX = (to.x-from.x)/duration;
         let speedY = (to.y-from.y)/duration;
-        this.move(from, to, speedX, speedY);
+        let speedZ = (to.z-from.z)/duration;
+        this.move(from, to, speedX, speedY, speedZ);
     }
+
     //Функция для движения с ускорением
-    //from, to - принимает {x:'значение', y:'значение'}
+    //from, to - принимает {x:'значение', y:'значение', z:'значение'}
     //startSpeed - начальная скорость  ед/cекунду
     //a - ускорение ед/cекунду2
-    moveToWithBoost = (from={x:0, y:0}, to={x:50, y:150}, startSpeed, a) => {
-        if(startSpeed <= 0) startSpeed = 0.1; //чтобы ускорение не становилось NaN
-        console.log(startSpeed, this.currentSpeedRes); //чтобы не тормозило при изменении направления
-        if(startSpeed < this.currentSpeedRes) startSpeed = this.currentSpeedRes;
-        let path = Math.pow((Math.pow((to.x-from.x), 2)+Math.pow((to.y-from.y), 2)), 0.5);
+    moveToWithBoost = (from={x:0, y:0, z:0}, to={x:50, y:150, z:0}, startSpeed, a) => {
+        if(startSpeed <= 0) startSpeed = 0.0001; //чтобы если скорость 0, ускорение в методе move() не было NaN
+        if(startSpeed < this.currentSpeedRes) startSpeed = this.currentSpeedRes;//чтобы не замедлялось при изменении направления
+        let path = Math.pow((Math.pow((to.x-from.x), 2) + Math.pow((to.y-from.y), 2) + Math.pow((to.z-from.z), 2)), 0.5);
         let startSpeedX = startSpeed * (to.x-from.x)/path;
         let startSpeedY = startSpeed * (to.y-from.y)/path;
-        this.move(from, to, startSpeedX, startSpeedY, a);
+        let startSpeedZ = startSpeed * (to.z-from.z)/path;
+        this.move(from, to, startSpeedX, startSpeedY, startSpeedZ, a);
     }
    
+    rotate = (angle) => {
+        this.r += angle;
+        this.r >= 360 ? this.r -= 360 : '';
+    }
+
     stopMovement = () => {
-        console.log('stop');
         clearInterval(this.interval);
     }
 }
 
+
+
+//класс для тестирования движения на плоскости (без поворота фигуры)
 class Board { 
     constructor(ctx) {
         this.ctx = ctx;
@@ -89,25 +102,28 @@ class Board {
             this.figure.stopMovement();
             this.moveFigTo(e);
         });
-        this.figure = new Figure(0, 0, 20, 20);
+        this.figure = new Figure(0, 0, 0, 20, 20, 0);
+        // this.radioBtn = document.querySelectorAll('#radioBtns input');
+        // this.radioBtn.addEventListener('change', () => {console.log('+')})
         this.draw();
     }
 
     moveFigTo = (e) => {
           let x = e.x-this.X;
           let y = e.y-this.Y;
-          console.log(x, y);
-        this.figure.moveToWithBoost({x:this.figure.x, y:this.figure.y}, {x:x, y:y}, 0, 10);
+        i
+          this.figure.moveToEvenly({x:this.figure.x, y:this.figure.y, z:0}, {x:x, y:y, z:0}, 0.2);  //равномерно
+        
+        this.figure.moveToWithBoost({x:this.figure.x, y:this.figure.y, z:this.figure.z}, {x:x, y:y, z:0}, 100, 100); //ускоренно
 
     }    
     draw = () => {
-        ctx.fillStyle = 'blue';
-        ctx.fillRect(this.figure.x, this.figure.y, this.figure.width, this.figure.height);
+        this.ctx.fillStyle = 'blue';
+        this.ctx.fillRect(this.figure.x, this.figure.y, this.figure.width, this.figure.height);
     }
     clear = () => {
         this.ctx.clearRect(0, 0, canvas.width, canvas.height);
     }
-
 
 }
 
---
+++ collection.js
@@ -0,0 +1,129 @@
+function Collection () {
+    //данные хранятся в формате { ID1:{key:value}, ID2:{key:value}, etc.}
+    //index - номер по-порядку как в массиве
+    let data = {};
+
+    this.set = (key, value) => {
+        let ID = Date.now() + Math.floor(Math.random()*150);
+        data[ID] = {[key]: value};
+        return data;     
+    }
+    this.hasID = (ID) => {
+        return ID in data ? true : false;
+    }
+    this.hasKey = (key) => {
+        for (ID in data) {
+            if(key in data[ID]) return true;
+            else continue;
+        }
+        return false;
+    }
+    this.hasIndex = (index) => {
+        return Object.entries(data)[index] ? true : false;
+    }
+    this.getDataArr = () => {
+        let dataArr = [];
+        for (ID in data) {
+            dataArr.push(data[ID])
+        }
+        return dataArr; // [{key:value}]
+    }
+    this.getDataObj = () => {
+        return data; // {ID:{key:value}}
+    }
+    this.size = () => {
+        return Object.entries(data).length;
+    }
+    this.getByID = (ID) => {
+        return data[ID] ? data[ID] : false;
+    }
+    this.hasIndex = (index) => {
+        return Object.entries(data)[index] ? true : false;
+    }
+    this.getByIndex = (index) => {
+        return Object.entries(data)[index];
+    }
+    this.removeById = (ID) => {
+        delete data[ID];
+    }
+    this.removeByKeyAll = (key) => {
+        //если несколько одинаковых ключей удаляет все
+        for (ID in data){
+           key in data[ID] ? delete data[ID] : ''
+        }
+        return this.get();
+    }
+
+    this.union = (...args) => {
+        args.forEach((dataObj) => 
+            data = {...data, ...dataObj.getDataObj()}
+        );
+        return this;
+    }
+
+    this.forEach = (callback)  => {
+        Object.entries(data).forEach((arr, index) => {
+            let key = Object.entries(arr[1])[0][0];
+            let value = Object.entries(arr[1])[0][1];
+            return callback (key, value, index);
+        });
+    }
+
+    this.unique = () => {
+        let dataRes = [];
+        for (ID in data) {
+            dataRes.push(data[ID]);
+        }
+        dataRes.forEach((elem, index) => {
+            if (dataRes.filter(el => JSON.stringify(el) === JSON.stringify(elem)).length > 1)
+                dataRes.splice(index, 1);
+        })
+        return dataRes;
+    }
+    this.uniqueKeys = () => {
+        let dataRes = [];
+        for (ID in data) {
+            dataRes.push(...Object.keys(data[ID]));
+        }
+        dataRes.forEach((elem, index) => {
+            if (dataRes.filter(el => el === elem).length > 1)
+                dataRes.splice(index, 1);
+        })
+        return dataRes;
+    }
+
+    this.sort = (callback) => {
+        let dataArr = [];
+        for (ID in data) {
+            dataArr.push(data[ID])
+        };
+        dataArr.sort(
+            (obj1, obj2) =>
+            {
+                let key1 = Object.entries(obj2)[0][0];
+                let key2 = Object.entries(obj1)[0][0];
+                let value1 = Object.entries(obj2)[0][1];
+                let value2 = Object.entries(obj1)[0][1];
+                return callback(value1, value2, key1, key2);
+            }
+        );
+        return dataArr;
+    }
+
+    this.sortID = (callback) => {
+        let dataArr = Object.entries(data);
+        console.log(dataArr);
+        dataArr.sort((obj1, obj2) => {
+            let ID1 = obj1[0];
+            let ID2 = obj2[0];
+            return callback(ID1, ID2);
+        });
+        return dataArr;
+    }
+    this.editByID = (ID, value) => {
+        let key = Object.keys(data[ID])[0];
+        data[ID][key] = value;
+
+    return this;
+    }
+}
